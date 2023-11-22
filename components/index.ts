d4eeedebe9b8687ccd2c69aa6c7eff9970833b0c
Palmn72 <cs.palmqvist@gmail.com>
2023-11-22 16:46:02 +0100

    exports header, productimage

--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,5 @@ export { default as HeroBanner } from "./HeroBanner";
 export { default as Navbar } from "./Navbar";
 export { default as Cart } from "./Cart";
 export { default as Product } from "./Product";
+export { default as Header } from "./Header";
+export { default as ProductImages } from "./ProductImages";
