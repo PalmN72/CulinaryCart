841c32dd437afb598ea2542b401605e74c2c2319
Palmn72 <cs.palmqvist@gmail.com>
2023-11-22 11:33:56 +0100

    extracted types to types file

---
+++ types/types.ts
@@ -0,0 +1,45 @@
+export type ProductT = {
+  image: [
+    {
+      _type: string;
+      _key: string;
+      asset: {
+        _ref: string;
+        _type: string;
+      };
+    }
+  ];
+  _createdAt: String;
+  name: String;
+  _updatedAt: String;
+  price: Number;
+  _rev: String;
+  _type: String;
+  details: String;
+  id: String;
+  slug: { current: String; _type: String };
+};
+
+export type BannerT = {
+  _rev: string;
+  discount: string;
+  midText: string;
+  smallText: string;
+  largeText2: string;
+  _id: string;
+  desc: string;
+  buttonText: string;
+  product: string;
+  _type: string;
+  _createdAt: string;
+  saleTime: string;
+  largeText1: string;
+  _updatedAt: string;
+  image: {
+    _type: string;
+    asset: {
+      _ref: string;
+      _type: string;
+    };
+  };
+};
