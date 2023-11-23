14f6ff5b397c7bd5883dd450d1a846ef0ec820c6
Palmn72 <cs.palmqvist@gmail.com>
2023-11-23 15:46:18 +0100

    cart components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import Link from "next/link";
-import { AiOutlineShopping } from "react-icons/ai";
-
+import { Cart } from ".";
+import CartIconButton from "./buttons/CartIconButton";
 const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
         <Link href="/">Apples</Link>
       </p>
-      <button type="button" className="cart-icon">
-        <AiOutlineShopping />
-        <span className="cart-item-qty">1</span>
-      </button>
+      <CartIconButton />
+      <Cart />
     </div>
   );
 };
