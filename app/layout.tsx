763386db3c2770f1bd899bee8f1af8148f013203
Palmn72 <cs.palmqvist@gmail.com>
2023-11-27 10:35:21 +0100

    fix: rename

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header, Footer } from "@/components";
-import { CartContextProvider } from "@/context/StateContext";
+import { CartContextProvider } from "@/context/CartContext";
 import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
