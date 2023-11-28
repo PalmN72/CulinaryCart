05cf2e10ecffb0067905a540e6342504e3cfb0a9
Palmn72 <cs.palmqvist@gmail.com>
2023-11-28 15:46:12 +0100

    fix kg price

--- components/Product.tsx
+++ components/Product.tsx
@@ -5,11 +5,11 @@ import { ProductT } from "@/types/types";
 import { urlForImage } from "@/sanity/lib/image";
 
 const Product: React.FC<{ product: ProductT }> = ({ product }) => {
-  const { slug, name, price } = product;
+  const { slug, name, price, category } = product;
 
   return (
     <div className="product-marquee">
-      <Link href={`/product/${slug.current}`}>
+      <Link href={`/products/${category}/${slug.current}`}>
         <div className="product-card">
           <img
             src={urlForImage(product.image[0]).width(250).height(250).url()}
@@ -17,7 +17,7 @@ const Product: React.FC<{ product: ProductT }> = ({ product }) => {
             className="product-image"
           />
           <p className="product-name">{name}</p>
-          <p className="product-price">SEK {price.toString()}</p>
+          <p className="product-price">SEK {price.toString()} / KG</p>
         </div>
       </Link>
     </div>
