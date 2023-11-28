8961da2c61c771fda05c6d78aab58ac91e46816b
Palmn72 <cs.palmqvist@gmail.com>
2023-11-28 15:45:59 +0100

    navbar paths

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,9 +5,11 @@ import CartIconButton from "./buttons/CartIconButton";
 const Navbar = () => {
   return (
     <div className="navbar-container">
-      <p className="logo">
-        <Link href="/">Apples</Link>
-      </p>
+      <Link href="/products/fruits">Fruit</Link>
+      <Link href="/products/vegetables">Vegetables</Link>
+      <Link href="/products/accessories">Accessories</Link>
+      <Link href="/about">About</Link>
+      <Link href="/delivery">Delivery</Link>
       <CartIconButton />
       <Cart />
     </div>
