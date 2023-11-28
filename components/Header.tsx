2f71a7a704e5e2bf6321d8caf4cfb1e62ff9f2a8
Palmn72 <cs.palmqvist@gmail.com>
2023-11-28 15:45:46 +0100

    placeholder for logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { Navbar } from ".";
+import Link from "next/link";
 
 const Header = () => {
-  return <Navbar />;
+  return (
+    <>
+      <p className="logo">
+        <Link href="/">Culinary Cart</Link>
+      </p>
+      <Navbar />
+    </>
+  );
 };
 
 export default Header;
