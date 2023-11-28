88ef6973c9e6308a8473b5cb3a571ff7625eebee
Palmn72 <cs.palmqvist@gmail.com>
2023-11-28 17:38:22 +0100

    test env

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,10 +1,11 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-11-21";
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
-);
+// export const dataset = assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_DATASET,
+//   "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
+// );
+export const dataset = process.env.SANITY_STUDIO_DATASET;
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
