bbb5e8fd75069fc15bd40345af04afcbeccb0454
Palmn72 <cs.palmqvist@gmail.com>
2023-11-28 16:07:48 +0100

    display fruits on landing

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,17 @@ const products = (await getProducts()).products;
 
 export default function Home() {
   return (
-    <main>
+    <main className="main">
       <HeroBanner heroBanner={bannerData} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Fruits</p>
       </div>
       <div className="products-container">
-        {products.map((product: ProductT) => (
-          <Product key={product._id as React.Key} product={product} />
-        ))}
+        {products.map((product: ProductT) => {
+          if (product.category === "fruits")
+            return <Product key={product._id as React.Key} product={product} />;
+        })}
       </div>
       <FooterBanner banner={bannerData} />
     </main>
