37ac7ab8b2c33600abcd49f2eaa7abdb7d88f17e
Palmn72 <cs.palmqvist@gmail.com>
2023-11-29 13:15:14 +0100

    price unit per category

--- components/Product.tsx
+++ components/Product.tsx
@@ -6,7 +6,7 @@ import { urlForImage } from "@/sanity/lib/image";
 
 const Product: React.FC<{ product: ProductT }> = ({ product }) => {
   const { slug, name, price, category } = product;
-
+  const unit = category === "accessories" ? "" : " / KG";
   return (
     <div className="product-marquee">
       <Link href={`/products/${category}/${slug.current}`}>
@@ -17,7 +17,11 @@ const Product: React.FC<{ product: ProductT }> = ({ product }) => {
             className="product-image"
           />
           <p className="product-name">{name}</p>
-          <p className="product-price">SEK {price.toString()} / KG</p>
+
+          <p className="product-price">
+            SEK {price.toString()}
+            {unit}
+          </p>
         </div>
       </Link>
     </div>
