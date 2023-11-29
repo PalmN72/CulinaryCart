eab4e46f4d073e8facbe39a5f3d03ecb0ec43320
Palmn72 <cs.palmqvist@gmail.com>
2023-11-29 14:16:57 +0100

    about page

---
+++ app/about/page.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="about-container">
+        <h3>about us</h3>
+
+        <p>
+          Welcome to Culinary Cart – Your Trusted Source for Organic Fresh
+          Produce!
+        </p>
+
+        <h2>Our Mission</h2>
+
+        <p>
+          <b>Nourishing You, Nourishing the Planet</b>
+        </p>
+
+        <p>
+          Our mission is simple but powerful – to provide you with access to the
+          healthiest and most flavorful organic produce while contributing to a
+          more sustainable planet. We source our fruits and vegetables directly
+          from local organic farms that share our commitment to eco-friendly
+          practices.
+        </p>
+        <h2>Our Promise to You</h2>
+
+        <p>
+          <b>Transparency and Customer Satisfaction</b>
+        </p>
+
+        <p>
+          <b>Traceability:</b> We believe in transparency. Each product in our
+          store comes with detailed information about its origin, ensuring you
+          know exactly where your food comes from.
+        </p>
+
+        <p>
+          <b>Customer Support:</b> Our dedicated customer support team is here
+          to assist you every step of the way.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
