3343fe5f7eb05d8b05e9968ebf2a50bc6e2b67ee
Palmn72 <cs.palmqvist@gmail.com>
2023-11-29 16:23:29 +0100

    moved link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { Navbar } from ".";
-import Link from "next/link";
 
 const Header = () => {
   return (
     <>
-      <p className="logo">
-        <Link href="/">Culinary Cart</Link>
-      </p>
       <Navbar />
     </>
   );
