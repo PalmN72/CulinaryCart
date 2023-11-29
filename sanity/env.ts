bc1fd64ed05f94ceb9909e3f31c2b1f3cea5ce12
Palmn72 <cs.palmqvist@gmail.com>
2023-11-29 12:25:25 +0100

    vercel test

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,22 +1,21 @@
-export const apiVersion = process.env.SANITY_STUDIO_API_VERSION || "2023-11-21";
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-11-21";
 
-export const dataset = "production";
-// export const dataset = assertValue(
-//   process.env.SANITY_STUDIO_DATASET,
-//   "Missing environment variable: SANITY_STUDIO_DATASET"
-// );
+export const dataset = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
+);
 
-export const projectId = "m9c5ybdd";
-// export const projectId = assertValue(
-//   process.env.SANITY_STUDIO_PROJECT_ID,
-//   "Missing environment variable: SANITY_STUDIO_PROJECT_ID"
-// );
+export const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
+);
 
 export const useCdn = true;
 
 export const token = assertValue(
-  process.env.SANITY_STUDIO_API_TOKEN,
-  "Missing environment variable: SANITY_STUDIO_API_TOKEN"
+  process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_API_TOKEN"
 );
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
